9ccaddbc4d60320bb111d27cf38337e71e1edb86
SpAcE Swings <ATZ_21@mail.ru>
2023-02-13 18:29:23 +0300

    13.02

--- src/main/java/by/ivankov/task5/entity/StopsGenerator.java
+++ src/main/java/by/ivankov/task5/entity/StopsGenerator.java
@@ -1,17 +1,19 @@
 package by.ivankov.task5.entity;
 
 import java.util.*;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class StopsGenerator {
-    ArrayDeque<BusStop> busStops;
+    BlockingDeque<BusStop> busStops;
     private Route route;
 
     public StopsGenerator() {
-        this.busStops = new ArrayDeque<>();
+        this.busStops = new LinkedBlockingDeque<>();
         this.route = Route.getInstance();
     }
 
-    public ArrayDeque<BusStop> generatorStops(int numberOfStops) {
+    public BlockingDeque<BusStop> generatorStops(int numberOfStops) {
         for (int i = 0; i < numberOfStops; i++) {
             busStops.add(new BusStop());
         }
